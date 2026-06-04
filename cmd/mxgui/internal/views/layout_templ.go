f714bd81da0704362ed23628365941363c732b68
infinage <37257700+Infinage@users.noreply.github.com>
2026-06-04 09:36:36 +0530

    GUI mockups: config update and minor enhancements

--- cmd/mxgui/internal/views/layout_templ.go
+++ cmd/mxgui/internal/views/layout_templ.go
@@ -57,7 +57,7 @@ func Layout() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</head><body class=\"bg-[#0a0d12] text-gray-300 h-screen overflow-hidden flex flex-col font-sans selection:bg-blue-900 selection:text-white\" x-data=\"{ \n          view: 'stream', /* 'stream', 'dictionary', 'toolbox', 'settings' */\n          inspectorOpen: false, \n          selectedMsg: null,\n          inspectorTab: 'decoded',\n          settingsTab: 'about',\n          toolboxTab: 'scripts',\n          sendRaw: false,\n          modal: '' /* '', 'seq', 'connect', 'form' */\n      }\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</head><body class=\"bg-[#0a0d12] text-gray-300 h-screen overflow-hidden flex flex-col font-sans selection:bg-blue-900 selection:text-white\" x-data=\"{ \n          view: 'stream', /* 'stream', 'dictionary', 'toolbox', 'settings' */\n          inspectorTab: 'decoded', /* 'decoded', 'validation', 'diff' */\n          settingsTab: 'about', /* 'about', 'config', 'aliases' */\n          toolboxTab: 'scripts', /* 'validate', 'scripts', 'finalize' */\n          modal: '', /* '', 'seq', 'connect', 'form', 'alias' */\n          inspectorOpen: false, \n          selectedMsg: null,\n          sendRaw: false,\n      }\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
